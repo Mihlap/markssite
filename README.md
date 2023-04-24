d736e6ab47ac056d026896e959e8e2dbc3bdbb13
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-04-24 17:04:55 +0300

    изменил navBar + добавил меню бургер

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Getting Started with Create React App
+# Marks Group main site
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+ Готовим сайт к выставке которая состояиться 24 мая 
+<!--
 ## Available Scripts
 
 In the project directory, you can run:
@@ -67,4 +67,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 
 ### `npm run build` fails to minify
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify) -->
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -10,55 +10,114 @@ const Navbar = () => {
 
   function handleClick() {
     setClicked(!clicked);
-   
-    navigate('/')
-    navigate('/competention')
-    navigate('/project"')
-    navigate('/public')
-    navigate('/company')
-    navigate('/contacts')
-    navigate('/polrtal')
+
   }
   return (
     <>
-    <Link to="/" className={`${styles.block} ${styles.block_logo}`} onClick={handleClick}>
-     <img src={logo} alt="logo"/>
-    </Link>
-    <Link to="/competention">
-    <button className={`${styles.block} ${styles.block1}`} onClick={handleClick}>
-    Компетенции
-    </button>
-    </Link>
-    <Link to="/project">
-    <button className={`${styles.block} ${styles.block2}`} onClick={handleClick}>
-    Проекты
-    </button>
-    </Link>
-    <Link to="/public">
-    <button className={`${styles.block} ${styles.block3}`} onClick={handleClick}>
-    Публикации
-    </button>
-    </Link>
-    <Link to="/company">
-    <button className={`${styles.block} ${styles.block4}`} onClick={handleClick}>
-    О Компании
-    </button>
-    </Link>
-    <Link to="/contacts">
-    <button className={`${styles.block} ${styles.block5}`} onClick={handleClick}>
-    Контакты
-    </button>
-    </Link>
-    <Link to="/portal">
-    <button className={`${styles.block} ${styles.block6}`} onClick={handleClick}>
-    HR портал
-    </button >
-    </Link>
-    <button className={`${styles.block} ${styles.block_lang}`}>
-       <p>EN</p>
-    </button>
-      </>
-      )
+      <div className={styles.navbar_desctop}>
+        <Link
+          to="/"
+          className={`${styles.block} ${styles.block_logo}`}
+          onClick={handleClick}
+        >
+          <img src={logo} alt="logo" />
+        </Link>
+        <Link to="/competention">
+          <button
+            className={`${styles.block} ${styles.block1}`}
+            onClick={handleClick}
+          >
+            Компетенции
+          </button>
+        </Link>
+        <Link to="/project">
+          <button
+            className={`${styles.block} ${styles.block2}`}
+            onClick={handleClick}
+          >
+            Проекты
+          </button>
+        </Link>
+        <Link to="/public">
+          <button
+            className={`${styles.block} ${styles.block3}`}
+            onClick={handleClick}
+          >
+            Публикации
+          </button>
+        </Link>
+        <Link to="/company">
+          <button
+            className={`${styles.block} ${styles.block4}`}
+            onClick={handleClick}
+          >
+            О Компании
+          </button>
+        </Link>
+        <Link to="/contacts">
+          <button
+            className={`${styles.block} ${styles.block5}`}
+            onClick={handleClick}
+          >
+            Контакты
+          </button>
+        </Link>
+        <Link to="/portal">
+          <button
+            className={`${styles.block} ${styles.block6}`}
+            onClick={handleClick}
+          >
+            HR портал
+          </button>
+        </Link>
+        <button className={`${styles.block} ${styles.block_lang}`}>
+          <p>EN</p>
+        </button>
+      </div>
+
+      <div className={styles.navDesktop_container}>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <nav>
+          <div className={styles.navbar}>
+            <div className={`${styles.container} ${styles.nav_container}`}>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <div className={styles.hamburger_lines}>
+                <span className={`${styles.line} ${styles.line1}`}></span>
+                <span className={`${styles.line} ${styles.line2}`}></span>
+                <span className={`${styles.line} ${styles.line3}`}></span>
+              </div>
+              <div className={styles.logo}>
+              </div>
+              <div className={styles.menu_items}>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">about</a>
+                </li>
+                <li>
+                  <a href="#">blogs</a>
+                </li>
+                <li>
+                  <a href="#">portfolio</a>
+                </li>
+                <li>
+                  <a href="#">contact</a>
+                </li>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
     }
 
 export default Navbar
