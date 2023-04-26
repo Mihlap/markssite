0328e0597a0eb28ac63f4e58daba01d06559507b
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-04-26 18:08:37 +0300

    скрол выключается по нажатию + navbar адаптирован на все экраны

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
@@ -10,9 +11,10 @@ import IconPlus from ".././icons/plus.svg";
 import show from ".././icons/show.svg";
 import styles from "./Header.module.css";
 import CSSPlugin from "gsap/CSSPlugin";
-import BlockHeader from '../UI/BlockHeader';
+import BlockHeader from "../UI/BlockHeader";
 
-
+// фото для телефона
+import photo from "./img/phone.svg";
 
 const settings = {
   dots: false,
@@ -30,7 +32,7 @@ const settings = {
   arrows: false,
   variableWidth: true,
 };
-const Header = () => {
+const Header = ({ handleClickScroll }) => {
   const [isFixed, setIsFixed] = useState(false);
 
   useEffect(() => {
@@ -47,38 +49,74 @@ const Header = () => {
       }
     }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     // <div className={styles.main}>
-      <div className={styles.header}>
-        <div className={styles.menu}>
-          <img className={styles.image} src="./image.png" alt="изображение" />
-        </div>
-        <Navbar />
-        <div className={styles.logo}>
-          <Slider className={isFixed ? `${styles.logo_fixed}` : `${styles.logo_slider}`} {...settings} centerPadding='5px'>
-            <div className={styles.item}><img src="./assets/1.png" alt="image1" /></div>
-            <div className={styles.item}><img src="./assets/2.png" alt="image2" /></div>
-            <div className={styles.item}><img src="./assets/3.png" alt="image3" /></div>
-            <div className={styles.item}><img src="./assets/4.png" alt="image4" /></div>
-            <div className={styles.item}><img src="./assets/5.png" alt="image5" /></div>
-            <div className={styles.item}><img src="./assets/6.png" alt="image6" /></div>
-            {/*добавлено для слайдера*/}
-            <div className={styles.item}><img src="./assets/1.png" alt="image1" /></div>
-            <div className={styles.item}><img src="./assets/2.png" alt="image2" /></div>
-            <div className={styles.item}><img src="./assets/3.png" alt="image3" /></div>
-            <div className={styles.item}><img src="./assets/4.png" alt="image4" /></div>
-            <div className={styles.item}><img src="./assets/5.png" alt="image5" /></div>
-            <div className={styles.item}><img src="./assets/6.png" alt="image6" /></div>
-          </Slider>
-        </div>
-        <div className={styles.test}>
+    <div className={styles.header}>
+      <div className={styles.menu}>
+        <img className={styles.image} src="./image.png" alt="изображение" />
+        <img className={styles.image_phone} src={photo} alt="photo" />
+      </div>
+      <Navbar handleClickScroll={handleClickScroll} />
+      <div className={styles.header_block}>
+        <h1 className={styles.heading}>Награды проектов</h1>
+        <p className={styles.heading_text}>
+          Наша компания участвует в<br /> многочисленных выставках, конференциях
+          <br /> и конкурсах. Предлагаем ознакомиться с<br /> некоторыми
+          докладами.
+        </p>
+      </div>
+      <div className={styles.logo}>
+        <Slider
+          className={isFixed ? `${styles.logo_fixed}` : `${styles.logo_slider}`}
+          {...settings}
+          centerPadding="5px"
+        >
+          <div className={styles.item}>
+            <img src="./assets/1.png" alt="image1" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/2.png" alt="image2" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/3.png" alt="image3" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/4.png" alt="image4" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/5.png" alt="image5" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/6.png" alt="image6" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/1.png" alt="image1" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/2.png" alt="image2" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/3.png" alt="image3" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/4.png" alt="image4" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/5.png" alt="image5" />
+          </div>
+          <div className={styles.item}>
+            <img src="./assets/6.png" alt="image6" />
+          </div>
+        </Slider>
+      </div>
+      {/* <div className={styles.test}>
           <button className={styles.show_svg}>
           <img src={show} alt="show" />
         </button>
@@ -121,13 +159,10 @@ const Header = () => {
          <div>
           <Footer />
         </div>
-      </div>
+      </div> */}
     </div>
     /* </div> */
   );
 };
-
-
-
 
 export default Header;
