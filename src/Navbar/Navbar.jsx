6725920123be3cb1f18425e75974b591b2b665c3
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-05-03 14:51:37 +0300

    немного изменил поведение navbar при открытии

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ handleClickScroll }) => {
   }
 
   const openHandler = () => {
-    setNavOpen(!navOpen);
+    setNavOpen((prevNavOpen) => !prevNavOpen);
   };
 
   const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
@@ -96,18 +96,13 @@ const Navbar = ({ handleClickScroll }) => {
         </button>
       </nav>
 
-      <div
-        className={styles.navDesktop_container}
-      >
-        {/* <div>
-          <img src={logo} alt="logo" />
-        </div> */}
+      <div className={styles.navDesktop_container}>
         <nav>
           <div className={styles.navbar}>
             <div className={styles.logo}>
               <img className={styles.logo_img} src={logo} alt="logo" />
             </div>
-            <div  className={`${styles.container} ${styles.nav_container}`}>
+            <div className={`${styles.container} ${styles.nav_container}`}>
               <input
                 className={styles.checkbox}
                 type="checkbox"
@@ -123,46 +118,53 @@ const Navbar = ({ handleClickScroll }) => {
                 <span className={`${styles.line} ${styles.line2}`}></span>
                 <span className={`${styles.line} ${styles.line3}`}></span>
               </div>
-              {navOpen && (
-                <>
-                  <div className={styles.menu_items}>
-                    <li>
-                      <Link to="#">Главная</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Компетенции</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Кейсы</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Публикации</Link>
-                    </li>
-                    <li>
-                      <Link to="#">О Компании</Link>
-                    </li>
-                    <li>
-                      <Link to="#">Контакты</Link>
-                    </li>
-                    <li>
-                      <Link to="#">HR портал</Link>
-                    </li>
-                    <div className={styles.contact_info}>
-                      <div className={styles.contact_info_block}>
-                        <div className={styles.contact_info_language}>
-                          EN | CN
-                        </div>
-                        <div className={styles.contact_info_mail}>
-                          mail@marksgroup.ru
-                        </div>
-                        <div className={styles.contact_info_phone}>
-                          +7 (495) 120-12-26
-                        </div>
-                      </div>
+              <div
+                style={{
+                  position: "fixed",
+                  top: 0,
+                  left: navOpen ? 0 : "100%", // Скрытие элемента за пределами экрана
+                  width: "100%",
+                  height: "100vh",
+                  backgroundColor: "#f6f6f6",
+                  transition: "left 0.5s ease-in-out", // Анимация скрытия/показа
+                  marginTop: "15%",
+                }}
+              >
+                <ul className={styles.menu_items}>
+                  <li>
+                    <Link to="#">Главная</Link>
+                  </li>
+                  <li>
+                    <Link to="#">Компетенции</Link>
+                  </li>
+                  <li>
+                    <Link to="#">Кейсы</Link>
+                  </li>
+                  <li>
+                    <Link to="#">Публикации</Link>
+                  </li>
+                  <li>
+                    <Link to="#">О Компании</Link>
+                  </li>
+                  <li>
+                    <Link to="#">Контакты</Link>
+                  </li>
+                  <li>
+                    <Link to="#">HR портал</Link>
+                  </li>
+                </ul>
+                <div className={styles.contact_info}>
+                  <div className={styles.contact_info_block}>
+                    <div className={styles.contact_info_language}>EN | CN</div>
+                    <div className={styles.contact_info_mail}>
+                      mail@marksgroup.ru
+                    </div>
+                    <div className={styles.contact_info_phone}>
+                      +7 (495) 120-12-26
                     </div>
                   </div>
-                </>
-              )}
+                </div>
+              </div>
             </div>
           </div>
         </nav>
