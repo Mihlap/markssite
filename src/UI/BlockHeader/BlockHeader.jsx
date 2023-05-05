cb2e1ad21b57edab2d4347574af5a1146c749dd5
Mihlap <mihlap@mail.ru>
2023-05-05 16:29:04 +0300

    практически полностью blockheader

--- src/UI/BlockHeader/BlockHeader.jsx
+++ src/UI/BlockHeader/BlockHeader.jsx
@@ -1,60 +1,98 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-// import { ScrollSmoother } from 'gsap/ScrollSmoother';
-import CSSPlugin from 'gsap/CSSPlugin';
+import { CSSPlugin } from 'gsap/CSSPlugin';
 import './index.scss';
 
 function BlockHeader() {
   gsap.registerPlugin(ScrollTrigger, CSSPlugin);
+  const accordionsRef = useRef(null);
+      
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-  useLayoutEffect(() => {
     const accordions = document.querySelectorAll('.accordion');
-    
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.accordions',
-        pinSpacing: true,
-        start: 'bottom bottom-=100',
-        end: 'bottom bottom',
-        scrub: false,
-        ease: 'linear',
-        immediateRender: false,
-        smoothScroll: true,
-        onLeaveBack: () => tl.reverse(
-          {
-            duration: 1.5
-          },
-        ),
-      }
-    });
 
     accordions.forEach((accordion) => {
-      tl.from(accordion.querySelector('.text'), {
-        height: 0,
-        paddingBottom: 0,
-        opacity: 0,
-        onComplete: () => {
-        accordion.style.height = 'auto';
+      const title = accordion.querySelector('.title');
+      const text = accordion.querySelector('.text');
+
+      
+
+      if (text.classList.contains('closed')) {
+        gsap.set(text, {
+          maxHeight: 0,
+          overflow: 'hidden',
+        });
+      } else {
+        gsap.set(text, {
+          maxHeight: text.scrollHeight,
+          overflow: 'hidden',
+        });
+      }
+
+     ScrollTrigger.create({
+        trigger: text,
+        start: 'bottom bottom-=150vh',
+        end: 'top bottom+=5%',
+        onEnter: () => {
+          gsap.to(text, {
+            maxHeight: text.scrollHeight,
+            duration: 1,
+          });
         },
-        duration: 1,
-       });
+        onLeaveBack: () => {
+          gsap.to(text, {
+            maxHeight: 0,
+            duration: 1,
+          });
+        },
+      });
+
+      ScrollTrigger.create({
+        trigger: accordion,
+        start: 'top bottom',
+        end: 'bottom bottom+=7%',
+        onEnter: () => {
+          if (!accordion.classList.contains('open')) {
+            gsap.to(text, {
+              maxHeight: 0,
+              duration: 1,
+            });
+          }
+        },
+      });
+
+      title.addEventListener('click', () => {
+        const isOpen = accordion.classList.contains('open');
+
+        if (isOpen) {
+          gsap.to(text, {
+            maxHeight: 0,
+            duration: 1,
+          });
+        } else {
+          gsap.to(text, {
+            maxHeight: text.scrollHeight,
+            duration: 1,
+          });
+        }
+
+        accordion.classList.toggle('open');
+      });
     });
 
-    tl.to('.accordion', {
-      marginTop: 1,
-      stagger: 0.5,
-     },
-      '<');
- }, []);
+  }, []);
+  
 
   return (
-    <div class="wrapper">
-      <div class="spacer"></div>
-      <div class="accordions">
-        <div class="accordion part1">
-          <div class="title">Архитектура</div>
-          <div class="text">
+    <div className="wrapper">
+      <div className="spacer"></div>
+      <div ref={accordionsRef} className="accordions">
+        <div className="accordion">
+          <div className="title">Архитектура</div>
+          <div className="text">
+            <div className="text_span">
            Архитектура и концепции
            <span>Разработка концепции, архитектурное проектирование -
             <br/>от идеи до ввода объекта в эксплуатацию</span>
@@ -67,11 +105,13 @@ function BlockHeader() {
            Проектная и рабочая документация
            <span>Прохождение экспертизы, авторский надзор, сопровождение
             <br/>строительства</span>
+            </div>
           </div>
         </div>
-        <div class="accordion part2">
-          <div class="title">Конструктив</div>
-          <div class="text">
+        <div className="accordion">
+          <div className="title">Конструктив</div>
+          <div className="text">
+            <div className="text_span">
             Инженерное проектирование
             <span>Разработка внутренних и наружных инженерных коммуникаций</span>
             Подземные транспортные сооружения
@@ -82,29 +122,46 @@ function BlockHeader() {
               Конструктивные решения
               <span>Разработка полного комплекса документации на всех стадиях
                 <br/>жизненного цикла проекта</span>
+                </div>
            </div>
         </div>
-        <div class="accordion part3">
-          <div class="title">Наука</div>
-          <div class="text">
+         <div className="accordion">
+          <div className="title">Наука</div>
+          <div className="text">
+            <div className="text_span">
           Научно-техническое сопровождение
           <span>Расчетное сопровождение проектирования и строительства
-            <br/>любой сложности
-          </span>
+              <br />любой сложности</span>
+              </div>
           </div>
         </div>
-        <div class="accordion part4">
-          <div class="title">Digital</div>
-          <div class="text">
+        <div className="accordion">
+          <div className="title">Digital</div>
+          <div className="text">
+            <div className="text_span">
             BIM моделирование
             <span>Проекты освоения подземного пространства для уникальных
               <br/>особо сложных объектов строительства</span>
+              </div>
           </div>
         </div>
-      </div>
-      <div class="spacer"></div>
+       </div>
+      <div className="spacer"></div>
     </div>
   )
 }
 
-export default BlockHeader
+export default BlockHeader;
+
+
+
+
+
+
+ 
+    
+    
+
+
+
+
