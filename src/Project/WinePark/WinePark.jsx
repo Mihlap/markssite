4572b81b3e836d0020217d6dcba61db97b46b572
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-05-05 12:26:43 +0300

    закончил с компонентов в котором  описанием проекта

--- src/Project/WinePark/WinePark.jsx
+++ src/Project/WinePark/WinePark.jsx
@@ -48,59 +48,70 @@ export default function WinePark() {
           </div>
           <div className="header_content">
             <div className="header_content_left">
-              <h2>Новый формат путешествий</h2>
-              <span>Генеральное проектирование</span>
-              <p>
+              <div className="title_block">
+                <h2 className="left_title">Новый формат путешествий</h2>
+                <div className="left_meta">Генеральное проектирование</div>
+              </div>
+              <div className="title_text">
                 Винный парк — это многофункциональное пространство для
-                энотуризма с собственными виноградниками и гравитационной
-                винодельней, остащённой самым современным оборудованием.
+                энотуризма с собственными<br/> виноградниками и гравитационной
+                винодельней, остащённой самым современным оборудованием.<br/>
                 Обширная парковая территория с различными элементами
-                благоустройства включает фруктовый, розовый и кактусовые сады,
-                зеркальный парящий мост, амфитеатр, водопад с гротом, малые
+                благоустройства включает фруктовый,<br/> розовый и кактусовые сады,
+                зеркальный парящий мост, амфитеатр, водопад с гротом, малые <br/>
                 архитектурные формы и концептуальные скульптуры.
-              </p>
+              </div>
 
-              <div>
-                <div>
-                  30 га
-                  <div>Общая площадь</div>
+              <div className="result_container">
+                <div className="result_block">
+                  <div className="result_number">30 га</div>
+                  <div className="result_text">Общая площадь</div>
                 </div>
-                <div>
-                  7. 75 га
-                  <div>Площадь виноградников</div>
+                <div className="result_block">
+                  <div className="result_number">7. 75 га</div>
+                  <div className="result_text">Площадь виноградников</div>
                 </div>
-                <div>
-                  54.5 м<div>Высота винной башни</div>
+                <div className="result_block">
+                  <div className="result_number">54.5 м</div>
+                  <div className="result_text">Высота винной башни</div>
                 </div>
               </div>
             </div>
             <div className="header_content_right">
-              <div>
-                <div>Расположение</div>
-                <div>г. Москва, Ленинградский проспект, вл. 37</div>
-              </div>
-              <div>
-                <div>Заказчик</div>
-                <div>-</div>
-              </div>
-              <div>
-                <div>Публикации</div>
-                <div>
-                  ТАСС «Первый в России винный парк откроется под Ялтой весной
-                  2021 года»
+              <div className="local_container">
+                <div className="local_ritgh">
+                  <div>
+                    <div className="local_title">Расположение</div>
+                    <div className="local_text">
+                      г. Москва, Ленинградский проспект, вл. 37
+                    </div>
+                  </div>
+                  <div>
+                    <div className="local_title">Заказчик</div>
+                    <div className="local_text">-</div>
+                  </div>
+                  <div>
+                    <div className="local_title">Публикации</div>
+                    <div className="local_text">
+                      ТАСС «Первый в России винный парк откроется под Ялтой
+                      весной 2021 года»
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <div>Степень участия</div>
-                <div>Генеральный проектировщик</div>
-              </div>
-              <div>
-                <div>Статус</div>
-                <div>В эксплуатации</div>
-              </div>
-              <div>
-                <div>Награды</div>
-                <div>-</div>
+                <div className="local_left">
+                  <div>
+                    <div className="local_title">Степень участия</div>
+                    <div className="local_text">Генеральный проектировщик</div>
+                  </div>
+                  <div>
+                    <div className="local_title">Статус</div>
+                    <div className="local_text">В эксплуатации</div>
+                  </div>
+                  <div>
+                    <div className="local_title">Награды</div>
+                    <div className="local_text">-</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
