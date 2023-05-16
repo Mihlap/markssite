129ad1177c115e45f152945ab49a13b87b8db13e
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-05-16 16:49:01 +0300

    push fix 0.4

--- src/UI/SliderHeader/SliderHeader.jsx
+++ src/UI/SliderHeader/SliderHeader.jsx
@@ -1,42 +1,41 @@
-import React from 'react';
+import React from "react";
 import Marquee from "react-fast-marquee";
-import styles from './SliderHeader.module.css';
+import styles from "./SliderHeader.module.css";
 
 const SliderHeader = () => {
   return (
-        <div className={styles.logo}>
-       <Marquee
-       gradient={true}
+    <div className={styles.logo}>
+      <Marquee
+        gradient={true}
         speed={80}
         direction="left"
-         spacing={10}
-         autoFill={true}
-         gaps={null} 
-         gradientColor={'#F6F6F6'}
-         delayBetweenLoops={2000}
-        >
-          <div className={styles.item}>
-        <img src="./assets/1.png" alt="image1" />
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/2.png" alt="image2" />
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/3.png" alt="image3" />
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/4.png" alt="image4" />
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/5.png" alt="image5" />
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/6.png" alt="image6" />
-      </div>
-     </Marquee>
-     </div>
-  )
-}
+        spacing={10}
+        autoFill={true}
+        gaps={null}
+        gradientColor={"#F6F6F6"}
+        delayBetweenLoops={2000}
+      >
+        <div className={styles.item}>
+          <img src="./assets/1.png" alt="image1" />
+        </div>
+        <div className={styles.item}>
+          <img src="./assets/2.png" alt="image2" />
+        </div>
+        <div className={styles.item}>
+          <img src="./assets/3.png" alt="image3" />
+        </div>
+        <div className={styles.item}>
+          <img src="./assets/4.png" alt="image4" />
+        </div>
+        <div className={styles.item}>
+          <img src="./assets/5.png" alt="image5" />
+        </div>
+        <div className={styles.item}>
+          <img src="./assets/6.png" alt="image6" />
+        </div>
+      </Marquee>
+    </div>
+  );
+};
 
-export default SliderHeader
-
+export default SliderHeader;
