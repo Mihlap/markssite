40a9eff426d3120d9dfe6e913cfa4ad27877f0bd
Mihlap <mihlap@mail.ru>
2023-05-17 13:51:55 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ const App = () => {
   const [isScrollDisabled, setIsScrollDisabled] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   // таймаут для прелоудера на сайте
 
-  // setTimeout(() => {
-  //   setLoading(false);
-  // }, 7000);
+  setTimeout(() => {
+    setLoading(false);
+  }, 7000);
 
   useEffect(() => {
     // Функция для отключения скролла
