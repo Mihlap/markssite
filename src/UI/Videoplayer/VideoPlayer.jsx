ee972ac2b2fb8db57f004ec9aad4fce9fb394ae7
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-05-18 10:56:09 +0300

    fix refactor video v0.0.8

--- src/UI/Videoplayer/VideoPlayer.jsx
+++ src/UI/Videoplayer/VideoPlayer.jsx
@@ -3,8 +3,18 @@ import styles from './Videoplayer.module.css';
 
 function VideoPlayer() {
   return (
-    <video className={styles.video_player} autoPlay muted loop>
-      <source src="./assets/PrimePark.mp4" type="video/mp4" className={styles.video_header} />
+    <video
+      className={styles.video_player}
+      playsInline
+      autoPlay
+      muted
+      loop
+    >
+      <source
+        src="./assets/PrimePark.mp4"
+        type="video/mp4"
+        className={styles.video_header}
+      />
     </video>
   );
 }
