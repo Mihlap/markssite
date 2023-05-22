2d5094af341ae77a57cae8dd0a01b2eeadbf2204
Mihlap <mihlap@mail.ru>
2023-05-22 18:10:17 +0300

    fix-ревью_конец1

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -178,7 +178,6 @@ export default function Header() {
         Проекты
       </div>
       <div
-        // ref={refs.swiperProgeject}
         className={styles.swiper_progect_container}
       >
         <SwiperContainerProgect />
