8ac063b6428e4a235d1595eb0c0feb64c06cec16
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-05-29 20:17:34 +0300

    fix post backend v0.1

--- frontend/src/test/Test.jsx
+++ frontend/src/test/Test.jsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchReviews } from "../store/Slice/reviewsSlice";
 import styles from "./Test.module.css";
 import LoadingCircle from "../Loading/LoadingCircle";
 
 export default function Test() {
+const [formData, setFormData] = useState({});
+
   const dispatch = useDispatch();
   const reviews = useSelector((state) => state.reviews.reviews);
   const loading = useSelector((state) => state.reviews.loading);
@@ -21,8 +23,22 @@ export default function Test() {
   if (error) {
     return <div>Error: {error}</div>;
   }
-
   console.log(reviews);
+
+   const handleChange = (event) => {
+     setFormData({
+       ...formData,
+       [event.target.name]: event.target.value,
+     });
+   };
+
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     console.log(formData);
+   };
+
+
+
 
   return (
     <div className={styles.test_container}>
@@ -30,12 +46,24 @@ export default function Test() {
       <div className={styles.test_block}>
         <div>
           <h1>Добавить пост</h1>
-          <font className={styles.test_form}>
-            <input type="text" placeholder="title" />
-            <input type="number" name="" id="" placeholder="rating" />
-            <input type="text" placeholder="body" />
-            <button>отправить </button>
-          </font>
+          <form className={styles.test_form} onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input type="text" name="name" onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+              Rating:
+              <input type="number" name="rating" onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+              Body:
+              <input type="text" name="body" onChange={handleChange} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
         </div>
         <div>
           <h1>Все посты</h1>
