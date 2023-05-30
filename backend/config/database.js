ddb7528ba962331886d09ace677a0c6c3d8bea28
Mihlap <mihlap@mail.ru>
2023-05-30 08:43:15 +0300

    about begin1

--- backend/config/database.js
+++ backend/config/database.js
@@ -28,6 +28,7 @@ module.exports = ({ env }) => {
     },
     mysql2: {
       connection: {
+        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
