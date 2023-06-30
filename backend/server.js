0ee7493acd51833cded110a1688007bf115399c5
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-06-30 15:19:36 +0300

    express start

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Старт 🏎️ 💨 ${PORT}`);
+});
