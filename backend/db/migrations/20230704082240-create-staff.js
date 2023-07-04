34b6e03e6213d49baae0a348f058014f787288b7
Mihlap <mihlap@mail.ru>
2023-07-04 12:21:43 +0300

    migration_all

---
+++ backend/db/migrations/20230704082240-create-staff.js
@@ -0,0 +1,67 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Staffs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      img: {
+        type: Sequelize.TEXT,
+      },
+      categorie: {
+        type: Sequelize.STRING,
+      },
+      position: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      about: {
+        type: Sequelize.TEXT,
+      },
+      about1: {
+        type: Sequelize.TEXT,
+      },
+      about2: {
+        type: Sequelize.TEXT,
+      },
+      about3: {
+        type: Sequelize.TEXT,
+      },
+      about4: {
+        type: Sequelize.TEXT,
+      },
+      about5: {
+        type: Sequelize.TEXT,
+      },
+      staffId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        allowNull: true,
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Staffs');
+  },
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -9,10 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Project, Articles }) {
+    static associate({ Project, Articles, Staff }) {
       // define association here
       this.hasMany(Project, { foreignKey: 'authorId' });
       this.hasMany(Articles, { foreignKey: 'authorId' });
+      this.hasMany(Staff, { foreignKey: 'staffId' });
     }
   }
   User.init({
