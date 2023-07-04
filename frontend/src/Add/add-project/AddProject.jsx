449b582181283e211f852db3f54b5b1cef6351c4
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2023-07-04 16:58:12 +0300

    backend v0.0.10

--- frontend/src/Add/add-project/AddProject.jsx
+++ frontend/src/Add/add-project/AddProject.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './AddProject.module.scss'
+import FormAddProject from './formAdd/FormAddProject';
 
 export default function AddProject() {
+const [isModalOpen, setIsModalOpen] = useState(true);
 
+const handleOpenModal = () => {
+  setIsModalOpen(true);
+};
+
+const handleCloseModal = () => {
+  setIsModalOpen(false);
+};
 
   return (
     <section className={styles.project_components_admin}>
       <div className={styles.project_components_admin__title}>Проекты</div>
 
-      <button className={styles.project_components_admin__button}>
-        <div>
+      <div
+        className={styles.project_components_admin__button_block}
+        onClick={handleOpenModal}
+      >
+        <button className={styles.project_components_admin__button}>
           <div>
             <svg
+              className={styles.project_components_admin__icon}
               width="41"
               height="40"
               viewBox="0 0 41 40"
@@ -30,15 +43,28 @@ export default function AddProject() {
                   id="Vector 10"
                   d="M10.5 20H20.5M30.5 20H20.5M20.5 20V10M20.5 20V30"
                   stroke="#75BBFD"
-                  stroke-width="2"
-                  stroke-linecap="square"
+                  strokeWidth="2"
+                  strokeLinecap="square"
                 />
               </g>
             </svg>
           </div>
-          <div>Добавить проект на главную</div>
+          <div className={styles.project_components_admin__button_text}>
+            Добавить проект на главную
+          </div>
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className={styles.project_components_admin__modal_container}>
+          <div className={styles.project_components_admin__modal_block}>
+            <FormAddProject />
+            <button
+              className={styles.project_components_admin__close_button}
+              onClick={handleCloseModal}
+            ></button>
+          </div>
         </div>
-      </button>
+      )}
     </section>
   );
 }
