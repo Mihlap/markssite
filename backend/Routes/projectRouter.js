f05adcbb59cbf51406c4ccf9e76a6247b38706c6
vladislav <cat@MacBook-Pro-vladislav.local>
2023-07-10 22:11:29 +0300

    backend v0.0.17

--- backend/Routes/projectRouter.js
+++ backend/Routes/projectRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line
 const { Project } = require("../db/models");
-const fileMiddleware = require('../middleware/file');
+const { uploadArray, uploadSingle } = require('../middleware/file');
 // const path = require('path');
 // const fs = require('fs');
 
@@ -18,31 +18,28 @@ router.get('/getzapros', async (req, res) => {
   }
 });
 
-router.post(
-  '/postzapros',
-  fileMiddleware.array('dropPhoto', 4),
-  async (req, res) => {
-    // эта консоль помогает нам понять, какие значения приходят на бекенд
+router.post('/postzapros', uploadArray, uploadSingle, async (req, res, next) => {
+  try {
+    const imageProjectResult = req.file ? req.file.filename : null;
 
-    try {
-      await Project.create({
-        title: req.body.title,
-        selectCompetencies: req.body.selectCompetencies,
-        countryCity: req.body.countryCity,
-        monthYear: req.body.monthYear,
-        viewConstruction: req.body.viewConstruction,
-        style: req.body.radioValue,
-        imageTitle: req.files.map((file) => file.filename).join(', '), // Объединяем имена фотографий через запятую
-        imageProject: req.file.filename,
-        // video: req.files.find((file) => file.fieldname === "dropVideo").filename,
-      });
-      res.status(200).json({ message: 'Данные успешно сохранены' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Произошла ошибка при сохранении данных' });
-    }
-  },
-);
+    await Project.create({
+      title: req.body.title,
+      selectCompetencies: req.body.selectCompetencies,
+      countryCity: req.body.countryCity,
+      monthYear: req.body.monthYear,
+      viewConstruction: req.body.viewConstruction,
+      style: req.body.radioValue,
+      imageTitle: req.files.map((file) => file.filename).join(', '),
+      imageProject: imageProjectResult,
+    });
+
+    res.status(200).send('Запись успешно сохранена!');
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 
 
 module.exports = router;
